c1dec74ada3ff7bbb319bc5413030bc3690738e1
Pavel Nedoshyvko <plight83@gmail.com>
2023-09-15 13:55:54 +0300

    feature/api-request

---
+++ src/js/api-request.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+export { serviceAllRecipes };
+axios.defaults.baseURL = "https://tasty-treats-backend.p.goit.global/api/recipes";
+
+
+async function serviceAllRecipes(page = 1, limit = 6) {
+	const params = new URLSearchParams({
+		title: '',
+		category: '',
+		area: '',
+		ingredient: '',
+		time: '',
+		page,
+		limit,
+	});
+	const { data } = await axios(`?${params}`);
+	return data;
+}
