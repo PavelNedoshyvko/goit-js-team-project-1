6f17d437689e03fac39ddd4ac4f560768888f411
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-09-16 13:59:25 +0200

    star reiting

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,12 @@
+
 const refs = {
   categoriesList: document.querySelector('.categories-list-js'),
   popularRecipesList: document.querySelector('.popular-recipes-list-js'),
   allCategoriesBtn: document.querySelector('.categories-btn-js'),
   swiperDiv: document.querySelector('.js-hero-slider'),
+  stars: document.querySelectorAll('.star-icon'),
+  starBox: document.querySelector('#starRating')
+
 };
 
 export { refs };
--- src/main.js
+++ src/main.js
@@ -5,3 +5,4 @@ import './js/mob-menu';
 import './js/eventsList';
 import './js/categories-list';
 import './js/popular-recipes-list';
+import './js/star';
