13d2dbae7bf07687a15292a4ff1b906bf81df9c4
KacanPasha <129742576+KacanPasha@users.noreply.github.com>
2023-09-21 14:24:42 +0300

    intermediate version of the category filter

--- src/js/favorites-icon.js
+++ src/js/favorites-icon.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', getHeart);
 
 function getHeart() {
   const elements = document.querySelectorAll('.btn-favorite');
-  console.log(elements);
+
   elements.forEach(item =>
     item.addEventListener('click', event => {
       // console.log(event);
