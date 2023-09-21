1e4ac9ab79eef02a9b0dbb34e52e52d044e61956
unknown <rubikonspace@ukr.net>
2023-09-21 11:22:09 +0300

    3

--- src/js/localeStorage.js
+++ src/js/localeStorage.js
@@ -1,28 +1,5 @@
+import axios from 'axios';
 import { serviceGetRecipeById } from './api-requests';
-
-export function getBtnFev() {
-  const addfevBtn = document.querySelector('.js-listener-fav-btn');
-
-  addfevBtn.addEventListener('click', evt => {
-    let clickIdRecipe = evt.target.dataset.id;
-    console.dir(clickIdRecipe);
-  });
-
-  // console.log(addfevBtn);
-}
-
-// function saveFavoriteCard(key, card) {
-//   const recipeCard = JSON.stringify(card);
-//   localStorage.setItem(key, save);
-// }
-
-// function getFavoriteCard(key) {
-//   return JSON.parse(localStorage.getItem(key));
-// }
-
-// function removeFavoriteCard(key) {
-//   localStorage.removeItem(key);
-// }
 
 const save = (key, value) => {
   try {
@@ -53,4 +30,51 @@ const remove = key => {
   }
 };
 
+export function getBtnFev() {
+  const addfevBtn = document.querySelector('.js-listener-fav-btn');
+
+  addfevBtn.addEventListener('click', evt => {
+    let clickIdRecipe = evt.target.dataset.id;
+    console.dir(clickIdRecipe);
+  });
+  // console.log(addfevBtn);
+}
+
 let arrayFav = [];
+
+function addFavCardRecipe(id) {
+  const itemRecipe = arrayFav.findIndex(_id =>
+    // _id === id
+    console.log(_id)
+  );
+
+  console.log(itemRecipe);
+  if (true) {
+    // переписати на bitWise
+    //
+    serviceGetRecipeById(id)
+      .then(data => {
+        arrayFav.push(data);
+        save('favIdCard', arrayFav);
+      })
+      .catch(() => {
+        console.log(error);
+      });
+  }
+}
+addFavCardRecipe('6462a8f74c3d0ddd28897fc1');
+addFavCardRecipe('6462a8f74c3d0ddd28897fc1');
+addFavCardRecipe('6462a8f74c3d0ddd28897fbc');
+
+// function saveFavoriteCard(key, card) {
+//   const recipeCard = JSON.stringify(card);
+//   localStorage.setItem(key, save);
+// }
+
+// function getFavoriteCard(key) {
+//   return JSON.parse(localStorage.getItem(key));
+// }
+
+// function removeFavoriteCard(key) {
+//   localStorage.removeItem(key);
+// }
