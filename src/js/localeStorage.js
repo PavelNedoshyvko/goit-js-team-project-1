568164baeaf3e7a89404ed6e51080102cd930fda
unknown <rubikonspace@ukr.net>
2023-09-21 13:52:33 +0300

    notif

--- src/js/localeStorage.js
+++ src/js/localeStorage.js
@@ -7,7 +7,7 @@ const save = (key, value) => {
     localStorage.setItem(key, serializedState);
   } catch (error) {
     console.error('Set state error: ', error.message);
-    // Notify.failure("Something went wrong. Please try again");
+    Notiflix.Notify('Something went wrong. Please try again');
   }
 };
 
@@ -17,7 +17,7 @@ const load = key => {
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
     console.error('Get state error: ', error.message);
-    // Notify.failure("Something went wrong. Please try again");
+    Notiflix.Notify('Something went wrong. Please try again');
   }
 };
 
@@ -26,7 +26,7 @@ const remove = key => {
     localStorage.removeItem(key);
   } catch (error) {
     console.error('Remove item error: ', error.message);
-    // Notify.failure("Something went wrong. Please try again");
+    Notiflix.Notify('Something went wrong. Please try again');
   }
 };
 
