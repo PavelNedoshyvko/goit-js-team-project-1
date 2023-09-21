2b6a4bf9c7af281db9f5cc53c7ee5d69cf4d10e8
SvitRia <129522432+SvitRia@users.noreply.github.com>
2023-09-21 09:39:10 +0200

    update pagination style. star js

--- src/js/star.js
+++ src/js/star.js
@@ -1,16 +1,49 @@
 //*star
 import { refs } from './refs';
 
-export function showReiting(obj) {
-  forEach(obj);
-  refs.stars.forEach(star => {
-    if (star.dataset.value <= Math.round(`${rating}`)) {
-      star.classList.add('star-active');
-    } else {
-      star.classList.remove('star-active');
-    }
-  });
+const gallaryItem = refs.mainList.childNodes;
+//const arrGallaryitem =  Array.from(gallaryItem);
+console.dir(gallaryItem);
+
+
+if (gallaryItem.readyState == 'loading') {
+    // ещё загружается, ждём события
+    gallaryItem.addEventListener('DOMContentLoaded', onWorkStar);
+  } else {
+    // DOM готов!
+    onWorkStar();
+  }
+console.log(gallaryItem);
+function onWorkStar(gallaryItem) {
+    gallaryItem.forEach((el) => {
+    let rating = el.querySelector('#starRating');
+    let stars = el.querySelectorAll('.star-icon')
+    stars.forEach(star => {
+        if (star.dataset.value <= (Math.round(rating))) {
+            star.classList.add('star-active');
+        } else {
+            star.classList.remove('star-active');
+        }
+      });
+
+ })
 }
+
+
+// //*star
+// import { refs } from './refs';
+
+// export function showReiting(obj) {
+//   obj.forEach(
+//   refs.stars.forEach(star => {
+//     if (star.dataset.value <= Math.round(`${rating}`)) {
+//       star.classList.add('star-active');
+//     } else {
+//       star.classList.remove('star-active');
+//     }
+//   }));
+// }
+
 //new
 function paintingStars() {
   const ratingStar = document.querySelector('.star-recipe-reting');
